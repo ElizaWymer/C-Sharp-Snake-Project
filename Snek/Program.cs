85f75c434405b3b9bfceccba97c4fb26ddeeaccd
Elizabeth <elizabethwymer2004@gmail.com>
2025-02-23 20:26:16 +0000

    Added tail growth and game over conditions

--- Snek/Program.cs
+++ Snek/Program.cs
@@ -10,24 +10,42 @@ int framdeDelayMilli = 100;
 
 Direction movementDirection = Direction.Still;
 
+List<Coord> snakePosHistory = new List<Coord>();
+int tailLength = 2;
+
+int score = 0;
+
 while (true)
 {
-    Console.Clear();
+    Console.WriteLine("Score: " + score + "\n");
+
     snakeHeadPos.ApplyMovementDirection(movementDirection);
 
     for (int y = 0; y < gridDimensions.Y; y++)
     {
+
         for (int x = 0; x < gridDimensions.X; x++)
         {
             Coord currentCoord = new Coord(x, y);
 
             if (snakeHeadPos.Equals(currentCoord))
             {
+
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
                 Console.Write("■");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (snakePosHistory.Contains(currentCoord))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("■");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (applePos.Equals(currentCoord))
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("a");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
             {
@@ -38,7 +56,31 @@ while (true)
                 Console.Write(" ");
             }
         }
+
         Console.WriteLine();
+    }
+
+    if (snakeHeadPos.Equals(applePos))
+    {
+        tailLength++;
+        applePos = new Coord(rand.Next(1, gridDimensions.X - 1), rand.Next(1, gridDimensions.Y - 1));
+        score += 1;
+    }
+    else if (snakeHeadPos.X == 0 || snakeHeadPos.X == gridDimensions.X - 1 || snakeHeadPos.Y == 0 || snakeHeadPos.Y == gridDimensions.Y - 1 || snakePosHistory.Contains(snakeHeadPos) && tailLength > 2)
+    {
+        score = 0;
+        tailLength = 2;
+        snakeHeadPos = new Coord(10, 10);
+        snakePosHistory = new List<Coord>();
+        movementDirection = Direction.Still;
+        applePos = new Coord(rand.Next(1, gridDimensions.X - 1), rand.Next(1, gridDimensions.Y - 1));
+        continue;
+    }
+    snakePosHistory.Add(new Coord(snakeHeadPos.X, snakeHeadPos.Y));
+
+    if (snakePosHistory.Count > tailLength)
+    {
+        snakePosHistory.RemoveAt(0);
     }
 
     DateTime time = DateTime.Now;
@@ -81,5 +123,7 @@ while (true)
             }
         }
     }
+    Console.Clear();
+
 }
 
