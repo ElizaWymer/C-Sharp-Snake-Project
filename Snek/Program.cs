ed616605c6bb830f414ea2c38a5b59bbe224ea31
Elizabeth <elizabethwymer2004@gmail.com>
2025-02-23 19:05:20 +0000

    Add project files.

---
+++ Snek/Program.cs
@@ -0,0 +1,26 @@
+﻿using Snek;
+
+Coord gridDimensions = new Coord(50, 20);
+Coord snakePos = new Coord(10, 10);
+
+for (int y =  0; y < gridDimensions.Y; y++)
+{
+    for (int x = 0; x < gridDimensions.X; x++)
+    {
+        Coord currentCoord = new Coord(x, y);
+
+        if (snakePos.Equals(currentCoord))
+        {
+            Console.Write("■");
+        }
+        else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+        {
+            Console.Write("#");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
